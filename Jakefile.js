125c66df8156667adfe557001dbaa7bc909d59e8
Lajos Koszti <ajnasz@ajnasz.hu>
2012-08-16 21:37:13 +0200

    Fix documentation for delData method in README

--- Jakefile.js
+++ Jakefile.js
@@ -1,6 +1,6 @@
 var spawn = require('child_process').spawn;
 
-desc('default', 'Run tests');
+desc('default', 'Run tests for ajn-session package');
 task('default', function (t) {
     spawn('node', ['tests/test.js'], {
         stdio: 'inherit'
